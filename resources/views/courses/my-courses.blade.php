d34eb6487aea81b55222591ac926726d53999373
iviieww <viwe4547@gmail.com>
2025-10-02 23:36:21 +0700

    create my-courese page
    create MyCoursescontroller
    update route
    update layouts.main (click for change page)
    update style.css

---
+++ resources/views/courses/my-courses.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main' , [
+    'My-courses'
+])
+
+@section('content')
+    <main>
+        <div class="container">
+            <h2>This is My courses page</h2>
+        </div>
+    </main>
+@endsection
