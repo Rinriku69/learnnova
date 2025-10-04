81f1df63a1211cb70b447340a836d0d9cfc9965a
iviieww <viwe4547@gmail.com>
2025-10-04 21:38:59 +0700

    create manage page
    create manage page and mockup data
    create ManageController

---
+++ resources/views/manage/manages.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main', [
+    'title' => 'Manage',
+])
+
+@section('content')
+    <main>
+        <div class="container">
+            <div class="wrapper">
+                <h2>this is manage page for admin</h2>
+            </div>
+        </div>
+    </main>
+@endsection
