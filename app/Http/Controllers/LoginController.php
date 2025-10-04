2a05a43730129f5d64a0fa666c1dd9fb1a548334
Rinriku69 <siripoohkim@gmail.com>
2025-10-04 21:54:34 +0700

    add login/auth

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\View\View;
+use Psr\Http\Message\ServerRequestInterface;
+
+class LoginController extends Controller
+{
+    function showForm(): View
+    {
+        return view('login.form');
+    }
+
+    function authenticate(ServerRequestInterface $request): RedirectResponse
+    {
+        $validator = Validator::make(
+            $request->getParsedBody(),
+            [
+                'email' => 'required',
+                'password' => 'required',
+            ],
+        );
+        if ($validator->passes() && Auth::attempt(
+            $validator->safe()->only(['email', 'password']),
+        )) {
+            session()->regenerate();
+            return redirect()->intended(route('home.main')); //route product.list id default
+        }
+
+        $validator
+            ->errors()
+            ->add(
+                'credentials',
+                'The provided credentials do not match our records.',
+            );
+
+        return redirect()->back()->withErrors($validator);
+    }
+
+    function logout(): RedirectResponse
+    {
+        Auth::logout();
+        session()->invalidate();
+
+        session()->regenerateToken();
+
+        return redirect()->route('login');
+    }
+
+    function registerForm() : View{
+        return view(
+            'login.register'
+        );
+    }
+
+    function register(ServerRequestInterface $request): RedirectResponse{
+        $data = $request->getParsedBody();
+
+        $user = new User();
+        $user->fill($data);
+        $user->email = $data['email'];
+        $user->role = 'USER';
+        $user->save();
+
+        return redirect()->route('login')
+        ->with('status','Successfully Registered please Login');
+
+
+    }
+}
