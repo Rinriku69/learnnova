16658472f51b230ef17dd88dd0c9646d514c2060
iviieww <viwe4547@gmail.com>
2025-10-04 21:39:49 +0700

    update etc
    update route/web.php
    edit style.css for front

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ManageController;
 use App\Http\Controllers\MyCoursesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.main');
 });
 
 
@@ -16,9 +18,24 @@ Route::controller(HomeController::class)
         route::get('', 'home')->name('main');
     });
 
+Route::controller(CourseController::class)
+    ->prefix('/course')
+    ->name('courses.')
+    ->group(static function (): void {
+        route::get('', 'Course')->name('course-info');
+        route::get('course', 'CourseDesc')->name('course-desc');
+    });
+
 Route::controller(MyCoursesController::class)
     ->prefix('/my-courses')
     ->name('my-courses.')
     ->group(static function (): void {
-        route::get('','MyCourses')->name('my-courses');
+        route::get('', 'MyCourses')->name('my-courses');
+    });
+
+Route::controller(ManageController::class)
+    ->prefix('/manage')
+    ->name('manage.')
+    ->group(static function (): void {
+        route::get('','Manage')->name('manage');
     });
