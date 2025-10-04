73655a3f9cacbabfe203ff3d42a46e52cf9da60f
iviieww <viwe4547@gmail.com>
2025-10-04 21:39:49 +0700

    update etc
    update route/web.php
    edit style.css for front

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ManageController;
 use App\Http\Controllers\MyCoursesController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -62,3 +64,36 @@ Route::middleware([
             });
         });
 });
+Route::get('/', function () {
+    return view('home.main');
+});
+
+
+Route::controller(HomeController::class)
+    ->prefix('/home')
+    ->name('home.')
+    ->group(static function (): void {
+        route::get('', 'home')->name('main');
+    });
+
+Route::controller(CourseController::class)
+    ->prefix('/course')
+    ->name('courses.')
+    ->group(static function (): void {
+        route::get('', 'Course')->name('course-info');
+        route::get('course', 'CourseDesc')->name('course-desc');
+    });
+
+Route::controller(MyCoursesController::class)
+    ->prefix('/my-courses')
+    ->name('my-courses.')
+    ->group(static function (): void {
+        route::get('', 'MyCourses')->name('my-courses');
+    });
+
+Route::controller(ManageController::class)
+    ->prefix('/manage')
+    ->name('manage.')
+    ->group(static function (): void {
+        route::get('','Manage')->name('manage');
+    });
