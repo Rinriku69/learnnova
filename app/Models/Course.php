1aef55210df7fcf89f3a10bf2f473763bab8e384
iviieww <viwe4547@gmail.com>
2025-10-04 23:14:43 +0700

    Add user-course relationship to models
    Defined the belongsTo relationship in Course and hasMany relationship in User to link courses to their respective users. Also set fillable fields and table name in Course model for better Eloquent integration.

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Course extends Model
 {
-    //
+    protected $table = 'courses';
+    protected $fillable = ['code', 'name', 'description', 'img', 'user_id',];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
