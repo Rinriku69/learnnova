e6c5e1e7233de7565845f65d2e61e11eaeb0e9e7
Rinriku69 <siripoohkim@gmail.com>
2025-10-08 20:20:37 +0700

    minor Change according to ford branch

--- resources/views/home/main.blade.php
+++ resources/views/home/main.blade.php
@@ -1,52 +1,46 @@
 @extends('layouts.main', [
-    'title' => 'Home'
+    'title' => 'Home',
 ])
+
+@section('header')
+    <div class="app-cmp-inf-header">
+        <div class="bar-vedeo-intro">
+            <iframe src="https://www.youtube.com/embed/DzjO4d9nh_k" title="JAVASCRIP" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen>
+            </iframe>
+            <span>WELCOME TO LEARNNOVA</span>
+        </div>
+        <button type="button" class="btn-more">See More...</button>
+    </div>
+@endsection
 
 @section('content')
     <main>
-        <div class="container">
-            <div class="app-cmp-inf-her">
-                <h1>WELCOME</h1>
-            </div>
-            <div class="app-cmp-inf-dt">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed dignissimos rerum nemo veniam sapiente
-                    voluptates tenetur similique esse asperiores optio porro repellat quisquam tempora, eos impedit sequi
-                    totam. Quia soluta, consequatur consequuntur ipsum excepturi officia doloremque dicta laboriosam
-                    adipisci id harum iste officiis saepe tenetur ratione quaerat ex molestias, ullam quos vitae sapiente,
-                    asperiores voluptate nulla? Dignissimos consequuntur provident sapiente quidem necessitatibus ea
-                    distinctio minima eum voluptatibus neque. Ratione architecto sint aperiam laboriosam neque commodi
-                    dolorum error facilis sequi nesciunt vero adipisci, nulla quo, eius porro, voluptas sed expedita. Ipsum
-                    odio ea enim quo libero officiis sequi corrupti, sit ipsa assumenda sint saepe unde voluptas eius
-                    veritatis tempora delectus deleniti at quasi ab iure cum dolor excepturi? Ducimus ipsum id molestiae rem
-                    tempora alias quae, fugiat tenetur laudantium corrupti voluptatibus suscipit iusto. Maiores
-                    exercitationem voluptate officia pariatur aperiam possimus obcaecati ducimus rem ab, tenetur explicabo
-                    soluta veniam sapiente inventore ipsam sint veritatis repellat quidem nesciunt, corporis quos fuga velit
-                    in! Numquam ex necessitatibus accusantium in dignissimos officia quisquam illum, quidem incidunt quia
-                    vel corporis dicta. A eveniet accusantium, odio, quo recusandae explicabo ex in labore ducimus amet sunt
-                    fugiat nemo at quasi id! Dolore ea quos itaque quam corrupti eveniet ex ut aliquid modi molestias
-                    officia tenetur culpa quidem amet ratione natus sequi perferendis possimus non, explicabo dolor. Id,
-                    soluta quisquam eius ullam, cum eos eaque blanditiis ducimus officiis animi totam et, vero repellendus
-                    placeat vel quas quaerat nostrum at quia repudiandae aspernatur? Quis illo eaque distinctio harum culpa
-                    fuga at dicta ad commodi, ea beatae tempore saepe optio odit nam consequatur eum et quibusdam facilis
-                    dolores sequi nulla explicabo. Blanditiis sequi hic alias voluptas ea corporis vitae quidem corrupti
-                    tempore beatae. Vero expedita repellat ipsam laborum porro, voluptatum architecto ratione dolorem
-                    excepturi sit ut adipisci consectetur blanditiis optio vitae totam earum incidunt soluta, est quae!
-                    Quidem excepturi voluptates nobis aliquam ut molestias perferendis voluptatem voluptate cumque illo
-                    dicta sunt facere ea ab consequatur ratione laboriosam explicabo, ipsam iure laudantium quaerat adipisci
-                    amet mollitia. Quo debitis veritatis nesciunt culpa iste aliquam reiciendis deleniti quas voluptatibus,
-                    quae dicta distinctio perspiciatis eos doloremque explicabo id assumenda quidem natus repudiandae. Magni
-                    at repellendus labore aliquid numquam? Dignissimos sequi alias pariatur reprehenderit quasi cupiditate
-                    sit dicta? Quaerat laudantium delectus voluptatem nisi dolorum quasi vel quisquam, cumque repudiandae ut
-                    facilis fugit totam asperiores, nostrum, officiis necessitatibus dolores pariatur officia numquam
-                    accusantium? Eius quia nostrum illum debitis sequi, nam fugiat beatae, adipisci, distinctio molestiae
-                    illo ad. Saepe aliquid quaerat sed laboriosam delectus consequatur hic accusamus soluta amet, blanditiis
-                    placeat aspernatur modi nostrum esse eius vero corrupti accusantium dolorem quos? Ex excepturi nisi
-                    voluptatibus dignissimos atque officia libero perferendis accusamus dolorem architecto. Tempora facilis
-                    perferendis nihil deserunt quae excepturi illo voluptates sit, tempore minima culpa cupiditate officiis
-                    maiores provident cum porro ipsum et nam exercitationem rerum eius odit eaque earum. Provident ipsum,
-                    dolorem ratione in odit quos molestiae modi repudiandae, deleniti, libero soluta illo et eveniet totam
-                    nemo accusantium a ex? Ullam laborum in tempora quasi delectus?</p>
-            </div>
+        <div class="app-cmp-inf">
+            <nav class="app-cmp-expcourses">
+                <dl class="app-cmp-coursdetail">
+                    <nav>
+                        <dt><img src="img/FDEV.jpg"></dt>
+                        <dd>FULL DEV</dd>
+                    </nav>
+
+                    <nav>
+                        <dt><img src="img/DS.jpg"></dt>
+                        <dd>DATA SCI</dd>
+                    </nav>
+
+                    <nav>
+                        <dt><img src="img/data_security.jpg"></dt>
+                        <dd>DATA SECURITY</dd>
+                    </nav>
+                </dl>
+            </nav>
+            <nav class="app-see-courses">
+                <a href="">
+                    All Course
+                </a>
+            </nav>
         </div>
     </main>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <title>{{ $title ?? 'E-Learning' }}</title>
+    <title>{{ $title ?? 'Learnnova' }}</title>
 </head>
 
-<body>
+<body id="app-cmp-body">
 
     <header class="nav">
         <nav>
