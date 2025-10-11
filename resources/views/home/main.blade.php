a8040911b14361348c91c33ca28e1045f7d776f4
Rinriku69 <siripoohkim@gmail.com>
2025-10-11 16:57:00 +0700

    Re origin

--- resources/views/home/main.blade.php
+++ resources/views/home/main.blade.php
@@ -5,10 +5,7 @@
 @section('header')
     <div class="app-cmp-inf-header">
         <div class="bar-vedeo-intro">
-           {{--  <iframe src="https://www.youtube.com/embed/DzjO4d9nh_k" title="JAVASCRIP" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen>
-            </iframe> --}}
+          
             <span>WELCOME TO LEARNNOVA</span>
         </div>
         <button type="button" class="btn-more">See More...</button>
@@ -19,22 +16,7 @@
     <main>
         <div class="app-cmp-inf">
             <nav class="app-cmp-expcourses">
-                <dl class="app-cmp-coursdetail">
-                    <nav>
-                        <dt><img src="img/FDEV.jpg"></dt>
-                        <dd>FULL DEV</dd>
-                    </nav>
-
-                    <nav>
-                        <dt><img src="img/DS.jpg"></dt>
-                        <dd>DATA SCI</dd>
-                    </nav>
-
-                    <nav>
-                        <dt><img src="img/data_security.jpg"></dt>
-                        <dd>DATA SECURITY</dd>
-                    </nav>
-                </dl>
+               
             </nav>
             <nav class="app-see-courses">
                 <a href="">
